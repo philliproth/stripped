6a028429daeca14142540e93a7fb79263ef26514
Phillip Roth <phillip@philliproth.de>
2018-06-12 16:01:00 +0200

    PHP 7 update

--- functions.php
+++ functions.php
@@ -54,8 +54,7 @@ add_theme_support('post-thumbnails');
 function stripped_scripts() {
 
     $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );    
 
 	// Comments Script threaded wp-includes/js/comment-reply.min.js
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -132,43 +131,64 @@ add_action('widgets_init', 'stripped_remove_recent_comments_style');
 
 
 class stripped_nav_menu extends Walker {
-    var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
-    var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
-    function start_lvl(&$output, $depth) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul>\n"; // List open
-    }
-    function end_lvl(&$output, $depth) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent</ul>\n"; // List close
-    }
-    function start_el(&$output, $item, $depth, $args) {
-        global $wp_query;
-        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-        $class_names = $value = '';
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-        $classes = in_array( 'current-menu-item', $classes ) ? array( 'current-menu-item' ) : array(); // you can change the current-menu-item class to something different or leave it blank ''
-        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-        $class_names = strlen( trim( $class_names ) ) > 0 ? ' class="' . esc_attr( $class_names ) . '"' : '';
-        $id = apply_filters( 'nav_menu_item_id', '', $item, $args );
-        $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-        $output .= $indent . '<li' . $id . $value . $class_names .'>'; //List item open
-        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-        $item_output = $args->before;
-        $item_output .= '<a'. $attributes .'>'; // Link open
-        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-        $item_output .= '</a>'; // Link close
-        $item_output .= $args->after;
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-    }
-    function end_el(&$output, $item, $depth) {
-        $output .= "</li>\n"; // List item close
-    }
-}
 
+  var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
+  function start_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<ul>\n";
+  }
+
+  function end_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "$indent</ul>\n";
+  }
+
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+    global $wp_query;
+    $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+    $class_names = $value = '';        
+    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+    /* Add active class */
+    if(in_array('current-menu-item', $classes)) {
+      $classes[] = 'active';
+      unset($classes['current-menu-item']);
+    }
+
+    /* Check for children */
+    $children = get_posts(array('post_type' => 'nav_menu_item', 'nopaging' => true, 'numberposts' => 1, 'meta_key' => '_menu_item_menu_item_parent', 'meta_value' => $item->ID));
+    if (!empty($children)) {
+      $classes[] = 'has-sub';
+    }
+
+    $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+    $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+    $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+    $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+    $output .= $indent . '<li>';
+
+    $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+    $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+    $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+    $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+
+    $item_output = $args->before;
+    $item_output .= '<a'. $attributes .'>';
+    $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+    $item_output .= '</a>';
+    $item_output .= $args->after;
+
+    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  }
+
+  function end_el( &$output, $item, $depth = 0, $args = array() ) {
+    $output .= "</li>\n";
+  }
+}
 
 
 
